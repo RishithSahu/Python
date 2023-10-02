8cadeff19c15ddeeb01395c5edc02f7f69dfcd28
RishithSahu <rishith.sahu@gmail.com>
2023-10-02 12:34:12 +0530

    completed?

--- SwObjs/user.py
+++ SwObjs/user.py
@@ -1,8 +1,27 @@
-# Python
+# A User object.
 class User:
-    def __init__(self,name,address,ph_no,balance):
+    def __init__(self,name,address,ph_no):
         self.username = name
         self.address = address
         self.Phone_No = ph_no
-        self.Acc_Balance = balance
-User_Instance = User("Rishith Sahu","Hoodi",9448028394,50000000)
+User_Instance = User("Rishith Sahu","Hoodi",9448028394)
+
+class Transactions:
+    def __init__(self,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10):
+        self.t1 = t1
+        self.t2 = t2
+        self.t3 = t3
+        self.t4 = t4
+        self.t5 = t5
+        self.t6 = t6
+        self.t7 = t7
+        self.t8 = t8
+        self.t9 = t9
+        self.t10 = t10
+List_of_Transactions = Transactions(1,2,3,4,5,6,7,8,9,10)
+
+
+class Balance:
+    def __init__(self,balance):
+        self.Balance = balance
+Total_Balance = Balance(5000000)
