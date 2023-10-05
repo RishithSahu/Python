742c734bfb4f6f188ccdb1746ab1770e5e97a01b
RishithSahu <rishith.sahu@gmail.com>
2023-10-05 12:58:27 +0530

    new

---
+++ Unit_2/Math.py
@@ -0,0 +1,14 @@
+# Library functions math and random.
+import math
+
+# print(dir(math))
+
+print(math.ceil(9999999.5))
+print(math.floor(9999999.5))
+print(round(10.55554))
+
+print(math.exp(10))
+print(math.pi)
+print(math.sqrt(2500))
+print(math.factorial(10))
+print(math.fabs(-1))
---
+++ Unit_2/Random.py
@@ -0,0 +1,11 @@
+# Random stuff
+import random
+print(random.random())
+# print(dir(random))
+print(random.randint(1,100))
+print(random.uniform(1,100))
+print(random.randrange(1,100,49))
+x=[1,2,3,4,5]
+random.shuffle(x)
+print(x)
+print(random.sample(x,2))
