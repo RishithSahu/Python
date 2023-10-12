d93bee5671071754d020c142e06480126c48dc59
RishithSahu <rishith.sahu@gmail.com>
2023-10-12 12:51:47 +0530

    new

---
+++ Unit_2/Set4.py
@@ -0,0 +1,8 @@
+# FROZEN SET
+tup = {11,12,13,14,1,2,3,4}
+
+se1 = {1,2,3,4,5}
+se1 = frozenset()
+# se1.add(23)  # AttributeError: 'frozenset' object has no attribute 'add'
+print(se1)
+# Rest is same.
