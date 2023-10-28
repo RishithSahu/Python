406a019199577734adbfa7dfbfa41c5fa0262e39
RishithSahu <rishith.sahu@gmail.com>
2023-10-28 09:53:52 +0530

    m

--- Basics/a_program_to_find_discount_on_a_no._of_items.py
+++ Basics/a_program_to_find_discount_on_a_no._of_items.py
@@ -1,4 +1,5 @@
-# A shop gives a  discount of 10%, if the cost of the quantity purchased is more than 1000 Ru, print total cost for the user.
+# A shop gives a  discount of 10%, if the cost of the quantity
+# purchased is more than 1000 Rs, print total cost for the user.
 
 quantity_of_items = int(input("Total no of items "))
 cost = 100*quantity_of_items
