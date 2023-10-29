156169015507e600c08f29a4b3caad218c65fcb6
RishithSahu <rishith.sahu@gmail.com>
2023-10-29 12:10:28 +0530

    m

--- Basics/reverseofnum.py
+++ Basics/reverseofnum.py
@@ -2,9 +2,11 @@
 
 num  = int(input("Enter Number"))
 reverse = 0
+if num == 0:
+     print("Number is 0")
 
 while (num!=0):
-rem=num%10
-reverse=reverse*10+rem
-num//=10
+     rem=num%10
+     reverse=reverse*10+rem
+     num//=10
 print("Reverse of the number = ",reverse)
