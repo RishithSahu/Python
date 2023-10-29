49536187814d1c8f00b2465433089b0b70b7c27d
RishithSahu <rishith.sahu@gmail.com>
2023-10-29 13:26:40 +0530

    m

--- Basics/reverseofnum.py
+++ Basics/reverseofnum.py
@@ -1,12 +1,14 @@
 # A program to reverse a number.
 
-num  = int(input("Enter Number"))
+num  = int(input("Enter Number "))
 reverse = 0
 if num == 0:
      print("Number is 0")
 
-while (num!=0):
+while (num!=0):    # This loop will run until the number is not equal to 0.
      rem=num%10
-     reverse=reverse*10+rem
      num//=10
+     reverse=reverse*10+rem
+     print(reverse)
+
 print("Reverse of the number = ",reverse)
