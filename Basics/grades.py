a026e122707866d3c0819e9a45d9b8c55cd64734
RishithSahu <rishith.sahu@gmail.com>
2023-10-29 10:32:14 +0530

    m

--- Basics/grades.py
+++ Basics/grades.py
@@ -1,12 +1,14 @@
 # Write a program to print grade A>70, print grade B>50, print grade C>35, print FAIL.
-marks = int(input("Enter marks "))
-
-if marks >= 70:
-    print("Grade A")
-elif marks >= 50:
-    print("Grade B")
-elif marks >= 35:
-    print("Grade C")
-else:
-    print("FAIL")
- 
+marks = input("Enter marks ").split()
+for i in range(len(marks)):
+    marks[i] = int(marks[i])
+    if marks[i] >= 90:
+       print("Grade A")
+    elif marks[i] >= 80:
+       print("Grade B")
+    elif marks[i] >= 70:
+       print("Grade C")
+    elif marks[i] >= 60:
+       print("Grade D")
+    else:
+       print("FAIL")
