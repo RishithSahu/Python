4a6cf222a222a444b7d86030a4e51749da0ca3c3
RishithSahu <rishith.sahu@gmail.com>
2023-11-10 11:01:22 +0530

    new

---
+++ Decorators.py
@@ -0,0 +1,5 @@
+#                                                     DECORATORS
+''' A powerful and useful tool in python since it 
+    allows you to modify the behavior of function or class
+ '''
+# 
