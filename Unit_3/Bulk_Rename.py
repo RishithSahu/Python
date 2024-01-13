269a4514f4b8c1092399f69d6678589a25866a76
RishithSahu <rishith.sahu@gmail.com>
2024-01-13 23:52:51 +0530

    m

--- Unit_3/Bulk_Rename.py
+++ Unit_3/Bulk_Rename.py
@@ -1,9 +1,9 @@
 import os
 def main():
-   i = 69
-   path = "C:/Users/Rishith/New Folder/Second Life Ranker/"               # path of the folder(but instead of \(up) use /(down))
+   i = 1
+   path = "E:/Rishith/Anime/DUHUSIAOSKQOIDJOWDFUWHDUHWOD/"               # path of the folder(but instead of \(up) use /(down))
    for filename in os.listdir(path):
-      my_dest = "Episode "+str(i)+".pdf"
+      my_dest = "Episode "+str(i)+".mp4"
       my_src = path + filename
       my_dest = path + my_dest
       os.rename(my_src, my_dest)
